a2bc31e38856ed5f27205551d61979bf058da8df
Ernest Bruno and Eric Swotinsky <eric@>
2018-01-09 13:20:52 -0800

    Preventing entire column from toggling on click. Restricted to individual cell.

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,6 @@
 $(document).ready(function() {
-
-  // $("h2").click(function() {
-  //   $("h2").toggle();
-  //   $("p").toggle();
-  // });
-  //
-  // $("p").click(function() {
-  //   $("h2").toggle();
-  //   $("p").toggle();
-  // });
-
-  $(".col-md-4:nth-child(1)").click(function(){
-    $(".col-md-4:nth-child(1) h2").toggle();
-    $(".col-md-4:nth-child(1) p").toggle();
+  $(".flashcard").click(function() {
+    $(this).children("h2").toggle();
+    $(this).children("p").toggle();
   });
-
-  $(".col-md-4:nth-child(2)").click(function(){
-    $(".col-md-4:nth-child(2) h2").toggle();
-    $(".col-md-4:nth-child(2) p").toggle();
-  });
-
-  $(".col-md-4:nth-child(3)").click(function(){
-    $(".col-md-4:nth-child(3) h2").toggle();
-    $(".col-md-4:nth-child(3) p").toggle();
-  });
-
-
-  });
+});
