3441bf6e2b3acd2dd52d311d2eca6cf55da8fe1e
Ernest Bruno and Eric Swotinsky <ernest@>
2018-01-09 13:05:51 -0800

    Fixed click to toggle functions. Added actual bootstrap.css into library.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,29 @@
-$().ready(function() {
+$(document).ready(function() {
 
-  $("h2").click(function() {
-    $("h2").toggle();
-    $("p").toggle();
-  })
+  // $("h2").click(function() {
+  //   $("h2").toggle();
+  //   $("p").toggle();
+  // });
+  //
+  // $("p").click(function() {
+  //   $("h2").toggle();
+  //   $("p").toggle();
+  // });
 
-  $("p").click(function() {
-    $("h2").toggle();
-    $("p").toggle();
+  $(".col-md-4:nth-child(1)").click(function(){
+    $(".col-md-4:nth-child(1) h2").toggle();
+    $(".col-md-4:nth-child(1) p").toggle();
+  });
 
-  })
-})
+  $(".col-md-4:nth-child(2)").click(function(){
+    $(".col-md-4:nth-child(2) h2").toggle();
+    $(".col-md-4:nth-child(2) p").toggle();
+  });
+
+  $(".col-md-4:nth-child(3)").click(function(){
+    $(".col-md-4:nth-child(3) h2").toggle();
+    $(".col-md-4:nth-child(3) p").toggle();
+  });
+
+
+  });
